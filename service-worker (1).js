a7d53074a4bbc5d151394f5cf2cffc8d3ec96b39
Kato-zero <catochibala@gmail.com>
2026-09-16 10:11:57 +0200

    Add files via upload

---
+++ service-worker (1).js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "nol-system-v2";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e)=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>{
+      return cache.addAll(FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>{
+      return Promise.all(
+        keys.map(k=>{
+          if(k!==CACHE_NAME){
+            return caches.delete(k);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e)=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>{
+      return res || fetch(e.request).catch(()=>{
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
